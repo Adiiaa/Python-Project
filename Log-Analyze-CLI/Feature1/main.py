be2474cc2cb3a97f67aad46d047e7a84b1bab780
adiiaa <adia.uwase24snhu@keplercollege.ac.rw>
2026-06-05 08:32:17 +0200

    feat2: parse plain test and JSON logs

--- Log-Analyze-CLI/Feature1/main.py
+++ Log-Analyze-CLI/Feature1/main.py
@@ -1,6 +1,30 @@
+import json
+
+def parse_log_line(line):
+    try:
+        log_data = json.loads(line)
+        timestamp= log_data['timestamp']
+        level = log_data['level']
+        message = log_data['message']
+        return timestamp, level, message
+    except json.JSONDecodeError:
+        parts = line.split()
+        timestamp = parts[0]+ " "+ parts[1]
+        level = parts[2]
+        message = " ".join(parts[3:])
+        return timestamp, level, message
+
 def read_log_file(file_path):
-    with open(file_path, 'r') as file:
-        for line_number, line in enumerate(file, start=1):
-            print(f"Line {line_number}: {line.strip()}")
+    try:
+        with open(file_path, 'r') as file:
+            for line_number, line in enumerate(file, start=1):
+                line = line.strip()
+                timestamp, level, message = parse_log_line(line)
+                print(f"\nLine {line_number}")
+                print(f"Timestamp: {timestamp}")
+                print(f"Level: {level}")
+                print(f"Message: {message}")
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found")
 if __name__ == '__main__':
     read_log_file("sample.log")
