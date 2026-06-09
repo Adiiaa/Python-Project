c913093c7521b2ced733e0e98b51a57b0a249740
adiiaa <adia.uwase24snhu@keplercollege.ac.rw>
2026-06-09 10:22:57 +0200

    feat: flexible config loading

--- ServerHealthChecker/health_checker/config.py
+++ ServerHealthChecker/health_checker/config.py
@@ -1,12 +1,23 @@
 import json
+import os
 
 
 def load_servers():
-    with open("config.json", "r") as file:
-        config = json.load(file)
 
-    servers = config["servers"]
+    env_servers = os.getenv("SERVERS")
 
-    print(f"Loaded {len(servers)} servers")
+    if env_servers:
+        servers = env_servers.split(",")
+        print(f"Loaded {len(servers)} servers from ENV")
+        return servers
 
-    return servers
+    try:
+        with open("config.json", "r") as file:
+            config = json.load(file)
+
+        servers = config["servers"]
+        print(f"Loaded {len(servers)} servers from file")
+        return servers
+
+    except FileNotFoundError:
+        raise Exception("No SERVERS env var or config.json found")
