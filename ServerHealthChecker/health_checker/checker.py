0c6b09261246fc54fc4c755dc1091e76fe6723a7
adiiaa <adia.uwase24snhu@keplercollege.ac.rw>
2026-06-09 08:45:15 +0200

    feat: measuring response time

--- ServerHealthChecker/health_checker/checker.py
+++ ServerHealthChecker/health_checker/checker.py
@@ -1,10 +1,18 @@
 import requests
+import time
 
 
 def check_server(url):
+    start = time.perf_counter()
+
     response = requests.get(url)
+
+    end = time.perf_counter()
+
+    response_time = (end - start) * 1000
 
     return {
         "url": url,
-        "status_code": response.status_code
+        "status_code": response.status_code,
+        "response_time": round(response_time, 2)
     }
